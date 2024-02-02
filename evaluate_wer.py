fe4ea343027ab3f9d5d55b5383b11722395cd9f0
Audran Bert <bert.audran@gmail.com>
2024-02-02 14:11:43 +0100

    improve wer script

--- evaluate_wer.py
+++ evaluate_wer.py
@@ -5,6 +5,8 @@ import numpy as np
 
 def load_data(data_path, ground_truth_folder):
     hardwares  = os.listdir(data_path)
+    files = {}
+    ground_truth_files = [x.split('.')[0] for x in os.listdir(ground_truth_folder)]
     for hardware in hardwares:
         devices = os.listdir(os.path.join(data_path, hardware))
         for device in devices:
@@ -14,18 +16,15 @@ def load_data(data_path, ground_truth_folder):
                 execs = [x for x in execs if not os.path.isfile(os.path.join(data_path, hardware, device, backend, x))]
                 for exec in execs:
                     transcripts_folder_path = os.path.join(data_path, hardware, device, backend, exec, 'transcripts')
-                    transcript_files = os.listdir(transcripts_folder_path)
-                    for k in transcript_files:
-                        k=k.split('.')[0]
+                    transcript_files = [x.split('.')[0] for x in os.listdir(transcripts_folder_path)]
+                    intersection = list(set(ground_truth_files) & set(transcript_files))
+                    for k in intersection:
                         if files.get(k) is None:
                             files[k] = {}
                         files[k][backend+'_'+exec] = os.path.join(transcripts_folder_path, k)
-    files = {}
-    ground_truth_files = os.listdir(ground_truth_folder)
-    ground_truth_files = [x.split('.')[0] for x in ground_truth_files]
-    for i in ground_truth_files:
+    for i in intersection:
         if files.get(i) is None:
-            print(f"Missing {i}")
+            print(f"Missing transcript for {i}")
             continue
         files[i]['ground_truth'] = os.path.join(ground_truth_folder, i + '.txt')
     return files
@@ -36,7 +35,7 @@ def load_prediction(file_path):
         line = f.readline()
         line = line.strip()
         if line.startswith("(None, None, '')"):
-            print('Empty prediction')
+            print(f'Empty prediction for {file_path}')
             return ''
         else:
             line = line.strip()
@@ -53,7 +52,7 @@ def process_wer(name, ref_file, pred_file, verbose=False):
     pred = load_prediction(pred_file)
     ref = load_truth(ref_file)
     # compute wer between ref and pred
-    wer_score = compute_wer([ref], [pred], normalization="fr")
+    wer_score = compute_wer([ref], [pred], normalization="fr", use_percents=True)
     if verbose:
         print(f"{name} WER: {wer_score['wer']:.2f}")
     return wer_score
@@ -63,7 +62,7 @@ def process_wer(name, ref_file, pred_file, verbose=False):
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--data_path', type=str, default='../normal_wer_')
+    parser.add_argument('--data_path', type=str, default='../faster_n_openai/normal_large_wer')
     parser.add_argument('--truth_folder', type=str, default="../ground_truths")
     args = parser.parse_args()
 
@@ -75,20 +74,21 @@ if __name__ == '__main__':
     data = load_data(data_path, truth_folder)
     os.makedirs('wer', exist_ok=True)
 
-    config_to_test = ['faster_int8_greedy_offline']
+    config_to_test = ['faster-whisper_int8_greedy_vad']
 
     wer_list = []
     for test in config_to_test:
         for i in data.keys():
-            wer_list.append(process_wer(i, data[i]['ground_truth'], data[i][test]))
+            wer_list.append(process_wer(i, data[i]['ground_truth'], data[i][test], verbose=False))
 
     wer_score_list = [x['wer'] for x in wer_list]
+    print()
     print(f"Number of files: {len(wer_score_list)}")
-    print(f"Mean WER: {sum(wer_score_list)/len(wer_score_list):.2f}")
+    print(f"Mean WER: {sum(wer_score_list)/len(wer_score_list):.2f}%")
     print(f"Std WER: {np.std(wer_score_list):.2f}")
-    print(f"Median WER: {sorted(wer_score_list)[len(wer_score_list)//2]:.2f}")
-    print(f"Min WER: {min(wer_score_list):.2f}")
-    print(f"Max WER: {max(wer_score_list):.2f}")
+    print(f"Median WER: {sorted(wer_score_list)[len(wer_score_list)//2]:.2f}%")
+    print(f"Min WER: {min(wer_score_list):.2f}%")
+    print(f"Max WER: {max(wer_score_list):.2f}%")
     
     # plot_wer(wer_list)
 
